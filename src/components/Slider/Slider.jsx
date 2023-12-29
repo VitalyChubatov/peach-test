fea923b59b3428c4f9035583905c0e947732b415
houldC <chubatovvv@bk.ru>
2023-12-29 13:16:25 +0300

    слайдер

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -8,11 +8,11 @@ export const Slider = () => {
   
   return (
 		<Carousel>
-			<div className={`${styles.img} ${styles.img1}`}></div>
-			<div className={`${styles.img} ${styles.img2}`}></div>
-			<div className={`${styles.img} ${styles.img3}`}></div>
-			<div className={`${styles.img} ${styles.img4}`}></div>
 			<div className={`${styles.img} ${styles.img5}`}></div>
+			<div className={`${styles.img} ${styles.img4}`}></div>
+			<div className={`${styles.img} ${styles.img3}`}></div>
+			<div className={`${styles.img} ${styles.img2}`}></div>
+			<div className={`${styles.img} ${styles.img1}`}></div>
 		</Carousel>
 	)
 }
